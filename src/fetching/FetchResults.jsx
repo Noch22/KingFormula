35e4c87058e969b94e5d99a50dc04e5f26ba21d2
Noch22 <noah.fakeadresse@gmail.com>
2025-02-28 23:59:38 +0100

    🐛 Fixing import bug

--- src/fetching/FetchResults.jsx
+++ src/fetching/FetchResults.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Cardresult from "../components/cardResult";
+import Cardresult from "../components/Cardresult";
 
 const FetchResults = () => {
   const [resultat, setResultat] = useState([]);
